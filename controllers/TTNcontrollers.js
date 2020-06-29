e8c9d320b6df8b72585c051b598c33df10b4c490
khanh.truongthuc@gmail.com <khanh.truongthuc@gmail.com>
2020-06-29 12:01:57 +0700

    everything-works-fine

--- controllers/TTNcontrollers.js
+++ controllers/TTNcontrollers.js
@@ -7,15 +7,6 @@ module.exports = {
     TTNPostData: async (req, res) => {
         try {
             console.log(req.body);
-
-            const deleteDate = await collectedData.find().select("created_at dateToExpire").sort({
-                created_at: -1
-            });
-            for (let i = 0; i < deleteDate.length; i++) {
-                deleteDate[i].dateToExpired = moment(`${deleteDate[i].created_at}`).fromNow();
-                console.log(moment(`${deleteDate[i].created_at}`).fromNow());
-                await Promise.all([deleteDate[i].save(), collectedData.deleteMany({dateToExpired: "10 days ago"})]);
-            }
             if (req.body.device_id == null) {
                 console.log("Thiếu ID nên bỏ nhé");
                 return res.status(400).json({
@@ -40,6 +31,14 @@ module.exports = {
                 console.log(idField);
                 const searchDevice = new collectedData(idField);
                 await Promise.all([searchDevice.save()]);
+                const deleteDate = await collectedData.find().select("created_at dateToExpire").sort({
+                    created_at: -1
+                });
+                for (let i = 0; i < deleteDate.length; i++) {
+                    deleteDate[i].dateToExpired = moment(`${deleteDate[i].created_at}`).fromNow();
+                    console.log(moment(`${deleteDate[i].created_at}`).fromNow());
+                    await Promise.all([deleteDate[i].save(), collectedData.deleteMany({dateToExpired: "10 days ago"})]);
+                }
                 return res.status(200).json({
                     type: "1",
                     data: searchDevice
